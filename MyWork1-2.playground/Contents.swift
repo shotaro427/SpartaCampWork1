1b293997bd9f5ece95e6c3404549e9023bebf7ee
shotaro427 <toshinobu.s111@gmail.com>
2019-08-05 10:46:54 +0900

    平日課題1-2の修正

--- MyWork1-2.playground/Contents.swift
+++ MyWork1-2.playground/Contents.swift
@@ -1,3 +1,10 @@
-import UIKit
 
-var str = "Hello, playground"
+// Int型定数
+let valueA:Int = 100
+let valueB:Int = 20
+
+// 上から順に足し算/引き算/掛け算/割り算
+print(valueA + valueB)
+print(valueA - valueB)
+print(valueA * valueB)
+print(valueA / valueB)
