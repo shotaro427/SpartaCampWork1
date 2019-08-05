6b52aa33eb1fa98fbdfe811ec526a6d59ba1aba3
shotaro427 <toshinobu.s111@gmail.com>
2019-08-05 11:40:59 +0900

    平日課題1-3の追加

--- MyWork1-3.playground/Contents.swift
+++ MyWork1-3.playground/Contents.swift
@@ -1,1 +1,13 @@
 
+// Int型変数
+var valueC: Int = 999999
+
+if (valueC / 10) == 0 {
+    print("\(valueC)は1桁です")
+} else if (valueC / 100) < 1 {
+    print("\(valueC)は2桁です")
+} else if (valueC / 1000) < 1 {
+    print("\(valueC)は3桁です")
+} else {
+    print("\(valueC)は4桁以上の値です")
+}
